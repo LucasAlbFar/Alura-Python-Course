84c555ab07f5368d2028291b73e70fadf4ca15eb
LucasAlbFar <lucasalbfar@gmail.com>
2020-02-20 22:25:33 -0300

    Create file guessing_game.py

---
+++ guessing_game.py
@@ -0,0 +1,19 @@
+print('+++++++++++++++++++++++++++++')
+print('Welcome to the Guessing Game!')
+print('+++++++++++++++++++++++++++++')
+
+secret_number = 87
+guess = int(input("Insert a number: "))
+print("Your guess is",guess)
+
+well_done = guess == secret_number
+greater   = guess > secret_number
+smaller   = guess < secret_number
+
+if (well_done):
+    print('Well Done :)')
+else:
+    if (greater):
+        print('Bad luck :( Your guess is greater than the secret number!')
+    elif (smaller):
+        print('Bad luck :( Your guess is smaller than the secret number!')
